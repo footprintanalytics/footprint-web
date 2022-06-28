675a6cf370a2a6ca30f48c2def0a4f208ba5e397
duke <yudapei@gmail.com>
2022-06-28 10:05:17 +0800

    feat: add home seo setting

--- frontend/src/metabase/containers/dashboards/index.jsx
+++ frontend/src/metabase/containers/dashboards/index.jsx
@@ -10,38 +10,38 @@ import Meta from "metabase/components/Meta";
 import MetaViewportControls from "metabase/dashboard/hoc/MetaViewportControls";
 import { compose } from "underscore";
 import title, { updateTitle } from "metabase/hoc/Title";
-import { uniq } from "lodash";
 import Creator from "./components/Creator";
 
 const Index = ({ router, user }) => {
-  const { category, tags } = router?.location?.query;
-  const defaultTitle = "Crypto Analysis Dashboards";
-  let defaultDescKeywords = ["GameFi", "NFTs", "Metaverse", "DeFi"];
+  const { category } = router?.location?.query;
+  const defaultTitle = "";
+  /*let defaultDescKeywords = ["GameFi", "NFTs", "Metaverse", "DeFi"];*/
 
   if (category) {
     updateTitle(category === "All" ? defaultTitle : `${category} Dashboards`);
-    if (category !== "All" && category !== "New") {
+    /*if (category !== "All" && category !== "New") {
       defaultDescKeywords = [category];
-    }
+    }*/
   } else {
     updateTitle(defaultTitle);
   }
 
-  if (tags) {
+  /*  if (tags) {
     if (typeof tags === "string") {
       defaultDescKeywords = uniq([...defaultDescKeywords, tags]);
     } else {
       defaultDescKeywords = uniq([...defaultDescKeywords, ...tags]);
     }
-  }
+  }*/
 
-  const defaultDesc = `Explore Blockchain Data about ${defaultDescKeywords.join(
-    ", ",
-  )}. A platform for discovering and visualizing blockchain data without coding.`;
+  const defaultDesc =
+    "Footprint is a powerful yet easy-to-use analytics tool to uncover and visualize blockchain data. The product puts user experience first whether you’re an analyst, data scientist, developer, student, teacher, or executive. It provides an intuitive, drag-and-drop interface for interactive data queries.";
+  const keywords = "Footprint";
+  const title = "Footprint Analytics: Crypto Analysis Dashboards";
 
   return (
     <>
-      <Meta description={defaultDesc} />
+      <Meta description={defaultDesc} keywords={keywords} title={title} />
       <div className="dashboards__wrap" data-nosnippet>
         <Recommendations />
         <div className="dashboards__layout">
