f99ff5f859601c2286c7ac6fd9b52271baebebde
qiuhongbingo <qiuhongbingo@outlook.com>
2022-07-08 15:38:25 +0800

    fix: no-unused-vars

--- frontend/src/metabase/containers/dashboards/components/Dashboards/nodata.jsx
+++ frontend/src/metabase/containers/dashboards/components/Dashboards/nodata.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import Icon from "metabase/components/Icon";
+// import Icon from "metabase/components/Icon";
 
 const NoData = () => {
   return (
