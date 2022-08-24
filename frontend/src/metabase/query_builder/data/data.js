cad28f900ac4350572b99eeefe26a78b09e72148
dukelight <yudapei@gmail.com>
2022-08-24 17:53:49 +0800

    fix-datefield-block-timestamp (#113)

--- frontend/src/metabase/query_builder/data/data.js
+++ frontend/src/metabase/query_builder/data/data.js
@@ -4,139 +4,139 @@ const dateFieldMapping = [
   //{chain}_token_transfers
   {
     tableName: "nft_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "arbitrum_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "avalanche_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "boba_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "bsc_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "celo_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "dfk_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "eos_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "ethereum_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "fantom_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "harmony_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "hive_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "hsc_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "iotex_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "moonbeam_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "moonriver_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "polygon_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "thundercore_transactions",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "arbitrum_token_transfers",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "avalanche_token_transfers",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "boba_token_transfers",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "bsc_token_transfers",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "celo_token_transfers",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "dfk_token_transfers",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "eos_token_transfers",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "ethereum_token_transfers",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "fantom_token_transfers",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "harmony_token_transfers",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "hsc_token_transfers",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "moonbeam_token_transfers",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "moonriver_token_transfers",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "polygon_token_transfers",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "solana_token_transfers",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
   {
     tableName: "thundercore_token_transfers",
-    dateField: "block_date",
+    dateField: "block_timestamp",
   },
 ];
 
