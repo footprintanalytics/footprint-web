b3a7889d10d66467b72a9e453e0b43c10711933a
duke <yudapei@gmail.com>
2022-10-10 10:34:59 +0800

    feat: button

--- frontend/src/metabase/containers/dataApi/components/DataApiButtons.jsx
+++ frontend/src/metabase/containers/dataApi/components/DataApiButtons.jsx
@@ -2,11 +2,24 @@
 import React from "react";
 import Link from "metabase/components/Link";
 import Button from "metabase/components/Button";
+import { push } from "react-router-redux";
+import { loginModalShowAction } from "metabase/redux/control";
+import { connect } from "react-redux";
 
-const DataApiButtons = ({ blandButtonText = "Try for free" }) => {
+const DataApiButtons = ({ user, setLoginModalShow, onChangeLocation, blandButtonText = "Try for free" }) => {
   return (
     <div className="data-api__buttons">
-      <Link to="mailto:sales@footprint.network" target="_blank">
+      <Link
+        to="mailto:sales@footprint.network"
+        target="_blank"
+        // onClick={() => {
+        //   if (user) {
+        //     onChangeLocation("/account/developer")
+        //   } else {
+        //     setLoginModalShow({ show: true, from: "Data api website" });
+        //   }
+        // }}
+      >
         <Button className="data-api__button-bland">{blandButtonText}</Button>
       </Link>
       <Link to="https://fp-api.readme.io/reference/welcome" target="_blank">
@@ -16,4 +29,15 @@ const DataApiButtons = ({ blandButtonText = "Try for free" }) => {
   );
 };
 
-export default DataApiButtons;
+const mapStateToProps = state => {
+  return {
+    user: state.currentUser,
+  };
+};
+
+const mapDispatchToProps = {
+  onChangeLocation: push,
+  setLoginModalShow: loginModalShowAction,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DataApiButtons);
