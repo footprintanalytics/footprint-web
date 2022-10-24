01d0508820905f3554492025eef0b86f8c82fa7f
duke <yudapei@gmail.com>
2022-10-24 12:24:52 +0800

    feat: ui

--- frontend/src/metabase/containers/dataApi/product/components/PowerfulData.jsx
+++ frontend/src/metabase/containers/dataApi/product/components/PowerfulData.jsx
@@ -48,7 +48,8 @@ const DomainDetail = () => {
               <li key={item.title}>
                 <img width={60} height={60} src={item.img} alt={item.title} />
                 <div className="powerful-data__right">
-                  <span>{item.title}</span>
+                  <h4>{item.title}</h4>
+                  <div className="mb2"/>
                   <span>{item.desc}</span>
                 </div>
               </li>
