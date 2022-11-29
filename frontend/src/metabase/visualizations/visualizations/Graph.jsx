f974409ad6d21c566621a7402b01de22c8b13849
qhbhq <qiuhongbingo@outlook.com>
2022-11-29 22:25:41 +0800

    fix: graph source & target

--- frontend/src/metabase/visualizations/visualizations/Graph.jsx
+++ frontend/src/metabase/visualizations/visualizations/Graph.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import { t } from "ttag";
 import _ from "underscore";
 import { columnSettings } from "metabase/visualizations/lib/settings/column";
+import { fieldSetting } from "metabase/visualizations/lib/settings/utils";
 import VizControls from "metabase/visualizations/hoc/VizControls";
 
 @VizControls
@@ -21,30 +22,30 @@ export default class Graph1 extends Component {
 
   static settings = {
     ...columnSettings({ hidden: true }),
-    // "graph.sourceField": {
-    //   section: t`Display`,
-    //   title: t`Source field`,
-    //   widget: "input",
-    //   default: "source",
-    // },
-    // "graph.sizeField": {
-    //   section: t`Display`,
-    //   title: t`Size field`,
-    //   widget: "input",
-    //   default: "size",
-    // },
-    // "graph.valueField": {
-    //   section: t`Display`,
-    //   title: t`Value field`,
-    //   widget: "input",
-    //   default: "value",
-    // },
-    // "graph.targetField": {
-    //   section: t`Display`,
-    //   title: t`Target field`,
-    //   widget: "input",
-    //   default: "target",
-    // },
+    ...fieldSetting("graph.sourceField", {
+      section: t`Data`,
+      title: t`Source Field`,
+      dashboard: false,
+      showColumnSetting: true,
+    }),
+    ...fieldSetting("graph.targetField", {
+      section: t`Data`,
+      title: t`Target Field`,
+      dashboard: false,
+      showColumnSetting: true,
+    }),
+    ...fieldSetting("graph.valueField", {
+      section: t`Data`,
+      title: t`Value Field`,
+      dashboard: false,
+      showColumnSetting: true,
+    }),
+    ...fieldSetting("graph.sizeField", {
+      section: t`Data`,
+      title: t`Size Field`,
+      dashboard: false,
+      showColumnSetting: true,
+    }),
     "graph.repulsion": {
       section: t`Display`,
       title: t`Repulsion`,
@@ -98,20 +99,38 @@ export default class Graph1 extends Component {
 
   getData() {
     const data = [];
+    const { settings } = this.props;
     const { cols, rows } = this.props.data;
-    const sourceIndex = cols.findIndex(c => c.name === "source");
-    const sizeIndex = cols.findIndex(c => c.name === "size");
-    const valueIndex = cols.findIndex(c => c.name === "value");
-    const targetIndex = cols.findIndex(c => c.name === "target");
-    rows.forEach(r => {
+    const sourceIndex = cols.findIndex(
+      c => c.name === settings["graph.sourceField"],
+    );
+    const sizeIndex = cols.findIndex(
+      c => c.name === settings["graph.sizeField"],
+    );
+    const valueIndex = cols.findIndex(
+      c => c.name === settings["graph.valueField"],
+    );
+    const targetIndex = cols.findIndex(
+      c => c.name === settings["graph.targetField"],
+    );
+    if (sourceIndex < 0 || sizeIndex < 0 || valueIndex < 0 || targetIndex < 0) {
+      return data;
+    }
+    rows.forEach((r, i) => {
       data.push({
+        id: i,
         name: r[sourceIndex],
         symbolSize: r[sizeIndex],
         value: r[valueIndex],
-        source: r[sourceIndex],
-        target: r[targetIndex],
+        source: i,
+        sourceRaw: r[sourceIndex],
+        targetRaw: r[targetIndex],
       });
     });
+    data.forEach(d => {
+      d.target = data.find(dd => dd.name === d.targetRaw)?.id;
+    });
+    console.log(data);
     return data;
   }
 
