88d6a569752032b6728610d13bb97104f75cc976
duke <yudapei@gmail.com>
2022-12-09 11:52:38 +0800

    feat: widgetbrand color

--- frontend/src/metabase/containers/WidgetBrand/index.jsx
+++ frontend/src/metabase/containers/WidgetBrand/index.jsx
@@ -1,11 +1,27 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import "./index.css";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
+import _ from "underscore";
 
-const WidgetBrand = () => {
+const WidgetBrand = ({ color }) => {
   const text = "Footprint";
 
-  return <div className="widget-brand">{text}</div>;
+  return (
+    <div className="widget-brand" style={{ color: color || "#dcdcdc" }}>
+      {text}
+    </div>
+  );
 };
 
-export default WidgetBrand;
+const mapStateToProps = (state, props) => {
+  return {
+    color: props?.location?.query?.color,
+  };
+};
+
+export default _.compose(
+  withRouter,
+  connect(mapStateToProps, null),
+)(WidgetBrand);
