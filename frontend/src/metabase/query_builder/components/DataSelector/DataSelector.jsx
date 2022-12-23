5bb8337d9252192848f279f819ff952be80b5914
duke <yudapei@gmail.com>
2022-12-23 10:10:11 +0800

    fix: code3

--- frontend/src/metabase/query_builder/components/DataSelector/DataSelector.jsx
+++ frontend/src/metabase/query_builder/components/DataSelector/DataSelector.jsx
@@ -1052,7 +1052,7 @@ export class UnconnectedDataSelector extends Component {
     /*if (this.isLoadingDatasets()) {
       return <LoadingAndErrorWrapper loading />;
     }*/
-    console.log("xxxxxx", this.hasDataAccess(), isSearchActive, isPickerOpen)
+
     if (this.hasDataAccess()) {
       return (
         <>
