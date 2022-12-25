b8fe3337d19f57d3094cee9066dc9ea69925e642
duke <yudapei@gmail.com>
2022-12-25 09:43:03 +0800

    fix: data api vip icon

--- frontend/src/metabase/nav/constants.ts
+++ frontend/src/metabase/nav/constants.ts
@@ -12,5 +12,10 @@ export const VipLevel = {
   ENTERPRISE: "enterprise",
 };
 
+export const VipLevelDataApi = {
+  GROWTH: "growth",
+  SCALE: "scale",
+};
+
 import { color, lighten } from "metabase/lib/colors";
 export const DefaultSearchColor = lighten(color("nav"), 0.07);
