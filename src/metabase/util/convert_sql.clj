4f9d7df3f863b162fbd87b1fcc56a5f27dce94a8
Jakin <jakin@footprint.network>
2023-02-21 20:07:18 +0800

    feat: account mapping dynamic

--- src/metabase/util/convert_sql.clj
+++ src/metabase/util/convert_sql.clj
@@ -1,7 +1,21 @@
 (ns metabase.util.convert_sql
   "Fp special business processing, data privacy isolation switch for special sql"
   (:require [clojure.string :as str]
-            [clojure.tools.logging :as log])
+            [clojure.tools.logging :as log]
+            [metabase.models.fga_account_mapping :as fga_account_mapping]
+            [metabase.models.fga_table :as fga_table])
+  )
+
+(defn get-fga-table-white-list []
+  (let [table-white-list (fga_table/fga-table-white-list)]
+    table-white-list
+    )
+  )
+
+(defn get-fga-account-mapping [groupName]
+  (let [account (fga_account_mapping/account-mapping-result groupName)]
+    (first account)
+    )
   )
 
 (defn get-footprint-schema []
@@ -19,7 +33,8 @@
   (println "handle-replace-schema" col)
   (let [trimTable (str/trim col)
         footprint-schema (get-footprint-schema)
-        fixedTrimTable (str/replace trimTable footprint-schema "animoca_mocaverse_test")]
+        account-schema (get-fga-account-mapping "animoca_mocaverse")
+        fixedTrimTable (str/replace trimTable footprint-schema account-schema)]
     (str/replace sql trimTable fixedTrimTable)
     )
   )
@@ -27,13 +42,16 @@
 (defn handle-add-schema [sql col]
   "Add schema if table is special"
   (println "handle-add-schema", col)
-  (let [trimTable (str/trim col)]
-    (str/replace sql trimTable (str "\"animoca_mocaverse_test\"." trimTable))
+  (let [trimTable (str/trim col)
+        account-schema (get-fga-account-mapping "animoca_mocaverse")
+        replaceSpaceSQLStr (str/replace sql (str trimTable " ") (str "\""  account-schema "\"." trimTable " "))
+        ]
+    (str/replace replaceSpaceSQLStr (str trimTable "\n") (str "\""account-schema "\"." trimTable "\n"))
     )
   )
 
 (defn handle-convert [sql col]
-  (let [fga-tables ["protocol_info"]
+  (let [fga-tables (into [] (get-fga-table-white-list))
         is_include (some #(str/includes? col %) fga-tables)]
     (if is_include
       (let [trimTable (str/trim col)
