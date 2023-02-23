1a0ee3e482f6524f20855abe2c5126417cc72405
eason <eason@footprint.network>
2023-02-23 19:26:16 +0800

    commit menus

--- frontend/src/metabase/growth/containers/Project.jsx
+++ frontend/src/metabase/growth/containers/Project.jsx
@@ -42,69 +42,87 @@ const Project = props => {
   }, [location.query.project_name]);
   const tabs_data = [
     {
-      name: "Overview",
+      name: "Project Overview",
       icon: React.createElement(ShopOutlined),
       id: null,
       children: null,
+    },
+    {
+      name: "Analytics",
+      icon: React.createElement(BarChartOutlined),
+      id: null,
+      children: [
+        { name: "New User", id: 7128 },
+        { name: "User Funnel", id: 7118 },
+        { name: "User Retention", id: 7119 },
+        { name: "User Profile", id: 7123 },
+        // { name: "Top Users", id: 7120 },
+        // { name: "User List", id: 7122 },
+        {
+          name: "NFT",
+          // icon: React.createElement(FileImageOutlined),
+          id: null,
+          children: [
+            { name: "NFT Overview", id: 7129 },
+            { name: "Holder", id: 7133 },
+            // { name: "Transaction", id: 7131 },
+            { name: "Profit Leadboard", id: null },
+          ],
+        },
+        {
+          name: "Competitors",
+          // icon: React.createElement(SearchOutlined),
+          id: null,
+          children: [
+            { name: "Industry Insights", id: null },
+            { name: "Competitive Comparison", id: null },
+            { name: "User Overlap", id: null },
+          ],
+        },
+        {
+          name: "Social Media",
+          // icon: React.createElement(CommentOutlined),
+          id: 7154,
+          // children: [
+          //   { name: "Social Dashboard", id: null },
+          //   { name: "Twitter", id: null },
+          //   { name: "Discord", id: null },
+          // ],
+        },
+      ],
     },
     {
       name: "Users",
       icon: React.createElement(TeamOutlined),
       id: null,
       children: [
-        { name: "New User", id: 7128 },
-        { name: "Top Users", id: 7120 },
-        { name: "User Funnel", id: 7118 },
-        { name: "User Retention", id: 7119 },
-        { name: "User Profile", id: 7123 },
-        { name: "User List", id: 7122 },
+        { name: "User List", id: 7136 },
+        { name: "Potential Users", id: null },
+        // { name: "Top Users", id: 7120 },
       ],
     },
     {
-      name: "NFT",
-      icon: React.createElement(FileImageOutlined),
+      name: "Cohorts",
+      icon: React.createElement(TeamOutlined),
       id: null,
-      children: [
-        { name: "NFT Overview", id: 7129 },
-        { name: "Holder", id: 7133 },
-        { name: "Transaction", id: 7131 },
-      ],
+      children: [{ name: "Cohort List", id: null }],
     },
     {
-      name: "Campaign Managemant",
-      icon: React.createElement(BarChartOutlined),
-      id: null,
-      children: [
-        { name: "Campaign List", id: null },
-        { name: "Locate Potential User", id: null },
-        { name: "Wallet Radar", id: null },
-        { name: "Snapshot", id: null },
-        { name: "Airdrop", id: null },
-      ],
-    },
-    {
-      name: "Social Media",
+      name: "Campaigns",
       icon: React.createElement(CommentOutlined),
       id: null,
       children: [
-        { name: "Social Dashboard", id: null },
-        { name: "Twitter", id: null },
-        { name: "Discord", id: null },
+        { name: "Campaign List", id: null },
+        // { name: "Locate Potential User", id: null },
+        // { name: "Wallet Radar", id: null },
+        // { name: "Snapshot", id: null },
+        { name: "Snapshot & Airdrop", id: null },
       ],
     },
-    {
-      name: "Competitive Research",
-      icon: React.createElement(SearchOutlined),
-      id: null,
-      children: [
-        { name: "Industry Insights", id: null },
-        { name: "Competitive Comparison", id: null },
-        { name: "User Overlap", id: null },
-      ],
-    },
+
     {
       name: "Custom Analysis",
-      icon: React.createElement(CustomerServiceOutlined),
+      icon: React.createElement(SearchOutlined),
       id: null,
       children: [{ name: "My Analysis", id: null }],
     },
@@ -117,43 +135,54 @@ const Project = props => {
   ];
 
   const dashboardMap = new Map();
-  const getTabs = () => {
+  const getTabs = tabs_data => {
     const tabs = [];
-    tabs_data.map(item => {
-      if (item.children && item.children.length > 0) {
-        const children = [];
-        item.children.map(child => {
-          children.push({
-            key: child.name,
-            icon: child.icon,
-            children: null,
-            label: child.name,
-          });
-          if (child.id) {
-            dashboardMap.set(child.name, child.id);
-          }
-        });
-        tabs.push({
-          key: item.name,
-          icon: item.icon,
-          children: children,
-          label: item.name,
-        });
-      } else {
-        tabs.push({
-          key: item.name,
-          icon: item.icon,
-          children: null,
-          label: item.name,
-        });
-        if (item.id) {
-          dashboardMap.set(item.name, item.id);
-        }
+    tabs_data?.map(item => {
+      const children = getTabs(item.children);
+      tabs.push({
+        key: item.name,
+        icon: item.icon,
+        children: children.length > 0 ? children : null,
+        label: item.name,
+      });
+      if (item.id) {
+        dashboardMap.set(item.name, item.id);
       }
+
+      // if (item.children && item.children.length > 0) {
+      //   const children = [];
+      //   item.children.map(child => {
+      //     children.push({
+      //       key: child.name,
+      //       icon: child.icon,
+      //       children: null,
+      //       label: child.name,
+      //     });
+      //     if (child.id) {
+      //       dashboardMap.set(child.name, child.id);
+      //     }
+      //   });
+      //   tabs.push({
+      //     key: item.name,
+      //     icon: item.icon,
+      //     children: children,
+      //     label: item.name,
+      //   });
+      // } else {
+      //   tabs.push({
+      //     key: item.name,
+      //     icon: item.icon,
+      //     children: null,
+      //     label: item.name,
+      //   });
+      //   if (item.id) {
+      //     dashboardMap.set(item.name, item.id);
+      //   }
+      // }
     });
     return tabs;
   };
-
+  console.log("getTabs(tabs_data)", getTabs(tabs_data));
   const getContentPannel = current_tab => {
     if (dashboardMap.has(current_tab)) {
       return (
@@ -181,7 +210,7 @@ const Project = props => {
         <GaSidebar
           router={router}
           location={location}
-          items={getTabs()}
+          items={getTabs(tabs_data)}
           currentProject={project}
         ></GaSidebar>
         <Content
