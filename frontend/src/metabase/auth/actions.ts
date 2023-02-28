b8cc50408bb16b24a93851b5e835d492a18ae0f5
dukelight <yudapei@gmail.com>
2023-02-28 11:27:23 +0800

    feat: quick login jump (#452)

--- frontend/src/metabase/auth/actions.ts
+++ frontend/src/metabase/auth/actions.ts
@@ -89,7 +89,7 @@ export const registerAndLogin = createThunkAction(
         setRegistSuccess(email);
       }
       MetabaseAnalytics.trackStructEvent("Auth", "registerAndLogin");
-      await handleLogin(dispatch, redirectUrl);
+      await handleLogin(dispatch, redirectUrl || "/");
     } else {
       throw message;
     }
