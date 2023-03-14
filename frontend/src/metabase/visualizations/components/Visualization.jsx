85bd59c401bc52d203ba882d00fe31f3d2bd3ddd
EasonChen <465680056@qq.com>
2023-03-14 20:22:59 +0800

    Feat growth analytics v2 (#515)

--- frontend/src/metabase/visualizations/components/Visualization.jsx
+++ frontend/src/metabase/visualizations/components/Visualization.jsx
@@ -50,6 +50,10 @@ import { memoizeClass } from "metabase-lib/utils";
 import { VisualizationSlowSpinner } from "./Visualization.styled";
 import CreateCohort from "./CreateCohort";
 import "./Visualization.css";
+import {
+  getGrowthProjectPath,
+  getLatestGAProject,
+} from "metabase/growth/utils/utils";
 
 // NOTE: pass `CardVisualization` so that we don't include header when providing size to child element
 
@@ -632,28 +636,16 @@ class Visualization extends React.PureComponent {
                     <Button
                       type="primary"
                       style={{ marginLeft: 20 }}
-                      href={"/growth?tab=Potential Users"}
-                      // onClick={() => {}}
+                      href={getGrowthProjectPath(
+                        getLatestGAProject(),
+                        "Potential Users",
+                      )}
                     >
                       All Users
                     </Button>
                   )}
                 </div>
               )}
-
-            {/* {(this.state.computedSettings["card.title"] ===
-              "Wallet User Stats" ||
-              this.state.computedSettings["card.title"] ===
-              "User Segmentation" ||
-              this.state.computedSettings["card.title"] === "All Cohorts")
-            && this.state.visualization?.identifier === "table"
-            && (
-              <CreateNotification state={this.state} style={{
-                position: "absolute",
-                left: 180,
-                top: 20,
-              }}/>
-            )} */}
             <CardVisualization
               {...this.props}
               // NOTE: CardVisualization class used to target ExplicitSize HOC
