25b91873300fcb99dfb1cc4d33ceba6f99893eac
eason <eason@footprint.network>
2023-04-10 16:02:14 +0800

    add Breadcrumb in some page

--- frontend/src/metabase/growth/containers/CohortList.jsx
+++ frontend/src/metabase/growth/containers/CohortList.jsx
@@ -31,7 +31,7 @@ const CohortList = props => {
       render: text => (
         <Link
           color="rgb(52, 52, 178)"
-          to={`/growth/public/dashboard/55b1eb29-b15e-458f-9241-1862a0d19d3b?tag=${text}`}
+          to={`/growth/public/dashboard/55b1eb29-b15e-458f-9241-1862a0d19d3b?tag=${text}#from=Cohort`}
         >
           {text}
         </Link>
@@ -60,13 +60,13 @@ const CohortList = props => {
         <Space size="middle">
           <Link
             color="rgb(52, 52, 178)"
-            to={`/growth/public/dashboard/55b1eb29-b15e-458f-9241-1862a0d19d3b?tag=${record.title}`}
+            to={`/growth/public/dashboard/55b1eb29-b15e-458f-9241-1862a0d19d3b?tag=${record.title}#from=Cohort`}
           >
             User Profile
           </Link>
           <Link
             color="rgb(52, 52, 178)"
-            to={`/growth/public/dashboard/dce33214-a079-4eb8-b53f-defaabde2eba?cohort_id=${record.cohortId}&cohort_title=${record.title}`}
+            to={`/growth/public/dashboard/dce33214-a079-4eb8-b53f-defaabde2eba?cohort_id=${record.cohortId}&cohort_title=${record.title}#from=Cohort`}
           >
             Wallet List
           </Link>
