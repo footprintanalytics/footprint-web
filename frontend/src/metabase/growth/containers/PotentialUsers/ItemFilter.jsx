a90b36f46d09c64d731aea572972e56604db3652
duke <yudapei@gmail.com>
2023-04-13 13:22:32 +0800

    feat: item filter

---
+++ frontend/src/metabase/growth/containers/PotentialUsers/ItemFilter.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from "react";
+import { Row, Col, Select } from "antd";
+import MuiSelect from "metabase/growth/components/MuiSelect";
+
+export const ItemFilter = ({ options }) => {
+  // mock datas
+  const optionsList = [
+    {
+      label: "Project",
+      options: [
+        {
+          value: 'The Sandbox',
+          label: 'The Sandbox',
+        },
+      ]
+    },
+    {
+      label: "NFT collection",
+      options: [
+        {
+          value: 'Moon Men',
+          label: 'Moon Men',
+        },
+      ]
+    },
+  ];
+  const onChange = (value) => {
+    console.log(`selected ${value}`);
+  };
+  return (
+    <div className="flex flex-row w-full p1 items-center">
+      <span style={{ marginRight: 8, color: "white", width: "80px", }}>Item Filter:</span>
+      <Row gutter={16} className="w-full">
+        {optionsList.map(item => (
+          <Col span={4} key={item.label}>
+            <MuiSelect
+              height={40}
+              style={{ width: "100%", height: 40 }}
+              label={item.label}
+              value={item.value}
+              onChange={onChange}
+              options={item.options}
+            />
+          </Col>
+
+        ))}
+      </Row>
+    </div>
+  );
+};
