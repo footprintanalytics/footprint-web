62c108f49ba3ef6643d30e84824fc2d84ebd3b18
qiuhongbingo <qiuhongbingo@outlook.com>
2023-04-14 12:44:39 +0800

    feat: delete filter out

--- frontend/src/metabase/containers/protocols/components/Protocols/CategoryForFga.jsx
+++ frontend/src/metabase/containers/protocols/components/Protocols/CategoryForFga.jsx
@@ -7,7 +7,13 @@ import { xor, union } from "lodash";
 import LoadingSpinner from "metabase/components/LoadingSpinner/LoadingSpinner";
 import { formatTableTitle } from "metabase/lib/formatting/footprint";
 
-const CategoryForFga = ({ data = [], isLoading, title, actives=[], onChange }) => {
+const CategoryForFga = ({
+  data = [],
+  isLoading,
+  title,
+  actives = [],
+  onChange,
+}) => {
   const categorys = union(actives, data);
   const otherIgnoreNum = 6 - actives.length;
   const getEnumArray = () => {
@@ -18,78 +24,93 @@ const CategoryForFga = ({ data = [], isLoading, title, actives=[], onChange }) =
       categorys.slice(0, otherIgnoreNum);
     }
     return [...actives, ...xor(categorys, actives).slice(0, otherIgnoreNum)];
-  }
+  };
 
   const getOthers = () => {
     return xor(categorys, getEnumArray());
-  }
+  };
 
   const others = getOthers();
   const enumArray = getEnumArray();
 
   const formatTitle = title => {
-    return formatTableTitle(title
-      ?.replace(/_wallets$/g, "")
-      ?.replace(/_wallet$/g, "")
-      ?.replace(/_users$/g, "")
-      ?.replace(/_marketplace_user$/g, "")
-      ?.replace(/_user$/g, "")
+    return formatTableTitle(
+      title
+        ?.replace(/_wallets$/g, "")
+        ?.replace(/_wallet$/g, "")
+        ?.replace(/_users$/g, "")
+        ?.replace(/_marketplace_user$/g, "")
+        ?.replace(/_user$/g, ""),
     );
-  }
+  };
 
   return (
     <div className="protocols__category-container">
-      {title && <h3 className="mr2" style={{
-        paddingBottom: 4
-      }}>{title}</h3>}
-      {isLoading ? (<LoadingSpinner size={16}/>) :
-        (
-          enumArray.length === 0 ? "No Column to filter on" :
-          <ul className="protocols__category">
-            {enumArray.map(item => (
-              <li
-                key={item}
-                onClick={() => trackStructEvent("Protocols Category", item)}
-              >
-                <div
-                  className={`fga__category-item ${
-                    actives.includes(item) ? "fga__category-item--active" : ""
-                  }`}
-
-                  onClick={e => {
-                    e.preventDefault();
-                    if (actives.includes(item)) {
-                      onChange(actives.filter(i => i !== item));
-                    } else {
-                      onChange([...actives, item]);
-                    }
-                  }}
-                >
-                  {formatTitle(item)}
-                </div>
-              </li>
-            ))}
-            {others && others.length > 0 && (
-              <Select
-                className="protocols__sort"
-                value="more"
-                placeholder="more"
-                showSearch
-                optionFilterProp="children"
-                filterOption={(input, option) => (option?.value?.toLowerCase() ?? '').includes(input.toLowerCase())}
-                onChange={value => {
-                  onChange([...actives, value]);
+      {title && (
+        <h3
+          className="mr2"
+          style={{
+            paddingBottom: 4,
+          }}
+        >
+          {title}
+        </h3>
+      )}
+      {isLoading ? (
+        <LoadingSpinner size={16} />
+      ) : enumArray.length === 0 ? (
+        "No Column to filter on"
+      ) : (
+        <ul className="protocols__category">
+          {enumArray.map(item => (
+            <li
+              key={item}
+              onClick={() => trackStructEvent("Protocols Category", item)}
+            >
+              <div
+                className={`fga__category-item ${
+                  actives.includes(item) ? "fga__category-item--active" : ""
+                }`}
+                onClick={e => {
+                  e.preventDefault();
+                  if (actives.includes(item)) {
+                    onChange(actives.filter(i => i !== item));
+                  } else {
+                    onChange([...actives, item]);
+                  }
                 }}
               >
-                {others.map(item => (
-                  <Select.Option key={item} value={item}>
-                    {formatTitle(item)}
-                  </Select.Option>
-                ))}
-              </Select>
-            )}
-          </ul>
-        )}
+                {/* {formatTitle(item)} */}
+                {item}
+              </div>
+            </li>
+          ))}
+          {others && others.length > 0 && (
+            <Select
+              className="protocols__sort"
+              value="more"
+              placeholder="more"
+              showSearch
+              optionFilterProp="children"
+              filterOption={(input, option) =>
+                (option?.value?.toLowerCase() ?? "").includes(
+                  input.toLowerCase(),
+                )
+              }
+              onChange={value => {
+                onChange([...actives, value]);
+              }}
+            >
+              {others.map(item => (
+                <Select.Option key={item} value={item}>
+                  {/* {formatTitle(item)} */}
+                  {item}
+                </Select.Option>
+              ))}
+            </Select>
+          )}
+        </ul>
+      )}
     </div>
   );
 };
