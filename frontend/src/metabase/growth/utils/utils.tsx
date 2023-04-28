1b08ba2f4b869a4b85aaac5538d0ed7903a7ec96
qiuhongbingo <qiuhongbingo@outlook.com>
2023-04-28 17:35:17 +0800

    fix: tag name format

--- frontend/src/metabase/growth/utils/utils.tsx
+++ frontend/src/metabase/growth/utils/utils.tsx
@@ -18,7 +18,7 @@ export function formatTag(tag: string) {
   for (let i = 0; i < words.length; i++) {
     words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
   }
-  return words.join(" ");
+  return words.join(" ").replaceAll("Nft", "NFT");
 }
 export function formatType(tag: string) {
   const words = tag.split(" ");
