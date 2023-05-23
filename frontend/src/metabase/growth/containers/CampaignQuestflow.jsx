241ed357dd484911c868e2e4a57f0ca1a6a54cdd
eason <eason@footprint.network>
2023-05-23 14:28:39 +0800

    commit

--- frontend/src/metabase/growth/containers/CampaignQuestflow.jsx
+++ frontend/src/metabase/growth/containers/CampaignQuestflow.jsx
@@ -18,46 +18,43 @@ const CampaignQuestflow = props => {
     },
     { ...QUERY_OPTIONS, enabled: !!project?.id && !!user?.id },
   );
-  console.log("params", type, project, user);
-  useEffect(() => {
-    if (!isLoading && data) {
-      console.log("loginQuestflow", data);
-    }
-  }, [data, isLoading]);
+
   const onPublish = flow => {
     console.log(flow);
     createQuestflowCampaign({
       projectId: parseInt(project?.id),
       campaignType: type,
       workflowId: flow.flowId,
-      webhook: flow.webhook
-    }).then(res => {
-      Modal.success({
-        content: 'Campaign created successfully!',
-        okText: 'View Campaigns',
-        onOk: () => {
-          router?.push({
-            pathname: `/growth/project/${project?.protocolSlug}/Campaign`,
-          });
-        }
-      });
-      console.log(res);
-    }).catch(err => {
-      console.log(err);
-    }).finally(() => {
-
-    });
+      webhook: flow.webhook,
+    })
+      .then(res => {
+        Modal.success({
+          content: "Campaign created successfully!",
+          okText: "View Campaigns",
+          onOk: () => {
+            router?.push({
+              pathname: `/growth/project/${project?.protocolSlug}/Campaign`,
+            });
+          },
+        });
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+      .finally(() => {});
   };
 
   return (
     <div className="flex flex-column items-center">
-      {isLoading ? (
+      {isLoading || data === null ? (
         <LoadingSpinner />
       ) : (
         <div className="w-full h-full">
           <Canvas
-            flowId={data?.flowId}
-            idToken={data?.id_token}
+            // flowName={''}
+            // flowDescription={''}
+            idToken={data?.idToken}
             // TODO: need a questflowURL in production
             // questflowURL={data?.questflowURL}
             onPublish={onPublish}
