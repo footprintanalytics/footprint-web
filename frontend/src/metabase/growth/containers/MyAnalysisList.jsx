b32dfb70a53d86427afeb2c1e17dd4dc72e14c1c
eason <eason@footprint.network>
2023-06-12 17:19:14 +0800

    add sign in alert in my analysis

--- frontend/src/metabase/growth/containers/MyAnalysisList.jsx
+++ frontend/src/metabase/growth/containers/MyAnalysisList.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import { connect } from "react-redux";
-import { message, Modal } from "antd";
+import { Alert, message, Modal, Button } from "antd";
 import List from "metabase/containers/creator/components/personal/list";
 import { getUser } from "metabase/selectors/user";
-import { createModalShowAction } from "metabase/redux/control";
+import {
+  createModalShowAction,
+  loginModalShowAction,
+} from "metabase/redux/control";
 
 const MyAnalysisList = props => {
-  const { router, location, children, user, project, setCreateModalShow } =
+  const { router, location, children, user, project, setCreateModalShow ,setLoginModalShow} =
     props;
   router.params.name = user?.name;
   const [messageApi, contextHolder] = message.useMessage();
@@ -17,20 +20,37 @@ const MyAnalysisList = props => {
     <div className="flex flex-column items-center justify-center w-full">
       {contextHolder}
       {modalContextHolder}
-      <div style={{ width: "100%" }}>
-        <List
-          router={router}
-          user={user}
-          name={user?.name}
-          location={router.location}
-        />
-      </div>
+      {user ? (
+        <div
+          className="my__analysis"
+          style={{ width: "100%", padding: "0px 20px" }}
+        >
+          <List
+            router={router}
+            user={user}
+            name={user?.name}
+            location={router.location}
+          />
+        </div>
+      ) : (
+        <div style={{ width: "100%", maxWidth: 600, margin: 20 }}>
+          <Alert
+            message={<Button type="primary" onClick={()=>{
+              setLoginModalShow({ show: true, from: "my_analysis" });
+            }}>Sign in</Button>}
+            description="Please sign in to view your analysis."
+            type="warning"
+            showIcon
+          />
+        </div>
+      )}
     </div>
   );
 };
 
 const mapDispatchToProps = {
   setCreateModalShow: createModalShowAction,
+  setLoginModalShow: loginModalShowAction,
 };
 
 const mapStateToProps = state => {
