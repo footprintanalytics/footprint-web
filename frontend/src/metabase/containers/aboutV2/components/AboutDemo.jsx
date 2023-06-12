34bc3dc2ac73b5acf4ac020d772cfb52da3dc678
dukelight <yudapei@gmail.com>
2023-06-12 10:24:49 +0800

    feat: home demo (#842)

--- frontend/src/metabase/containers/aboutV2/components/AboutDemo.jsx
+++ frontend/src/metabase/containers/aboutV2/components/AboutDemo.jsx
@@ -5,51 +5,90 @@ import AboutButton from "metabase/containers/aboutV2/components/AboutButton";
 import AboutImage from "metabase/containers/aboutV2/components/AboutImage";
 
 const AboutDemo = () => {
+  const data = [
+    {
+      title: "Research Portal",
+      desc: "Built by the researcher team for researchers",
+      content: <span>A one-stop data research tool covering GameFi, NFT, and Chain, unlocking your <br />capabilities from industry, to chain ecosystem to individual projects and more.</span>,
+      buttons: [
+        {
+          buttonText: "Explore Research Portal",
+          link: "https://www.footprint.network/research/gamefi",
+        }
+      ],
+      image: "home-v2/img-demo-1-1.png",
+      bg: "home-v2/img-demo-1-2.png",
+    },
+    {
+      title: "Growth Analytics",
+      desc: "One platform for your team and user growth",
+      content: <span>Dive into data insights and get an edge in your marketing strategy with <br/>Footprint GA by bringing all of your Web2 and Wed3 data sources together.</span>,
+      buttons: [
+        {
+          buttonText: "Explore Analytics GA",
+          link: "https://www.footprint.network/growth",
+        }
+      ],
+      image: "home-v2/img-demo-2-1.png",
+      bg: "home-v2/img-demo-2-2.png",
+    },
+    {
+      title: "Analytics APP",
+      desc: "Zero coding analytics as Tableau for crypto",
+      content: <span>Use Footprint real-time on-chain data and uploaded data to build charts and <br/>dashboards that showcase cutting-edge blockchain market insights without code <br/>using a drag-and-drop interface as well as with SQL.</span>,
+      buttons: [
+        {
+          buttonText: "Explore Analytics APP",
+          link: "https://www.footprint.network/dashboards",
+        }
+      ],
+      image: "home-v2/img-demo-3-1.png",
+      bg: "home-v2/img-demo-3-2.png",
+    },
+    {
+      title: "Data API",
+      desc: "Build your application with the Data API",
+      content: <span>A unified data API for NFTs, GameFi, and DeFi across all major chain ecosystems. <br/>Support not only raw data but also statistics metrics with one line of code.</span>,
+      buttons: [
+        {
+          buttonText: "Explore Data API",
+          link: "https://www.footprint.network/data-api",
+        },
+        {
+          buttonText: "Batch Download",
+          link: "https://www.footprint.network/batch-download",
+        }
+      ],
+      image: "home-v2/img-demo-4-1.png",
+      bg: "home-v2/img-demo-4-2.png",
+    },
+  ];
+
   return (
     <div className="About__demo">
-      <div className="About__demo-growth-analytics">
-        <div className="About__demo-growth-analytics-left">
-          <h3>Growth Analytics</h3>
-          <h4>One platform for your team and user growth</h4>
-          <span>Dive into data insights and get an edge in your marketing strategy with <br/>Footprint GA by bringing all of your Web2 and Wed3 data sources together.</span>
-          <AboutButton
-            className="mt4"
-            buttonText="Explore Footprint GA"
-            link="https://www.footprint.network/growth"
-          />
-        </div>
-        <AboutImage className="About__demo-growth-analytics-image" src={getOssUrl("home-v2/img_growth_analytics.png")} alt="Growth Analytics" />
-      </div>
       <div className="About__demo-bottom">
-        <div className="About__demo-analytics-app">
-          <h3>Analytics APP</h3>
-          <h4>Zero coding analytics as Tableau for crypto</h4>
-          <span>Use Footprint real-time on-chain data and uploaded data to build charts and dashboards that showcase cutting-edge blockchain market insights without code using a drag-and-drop interface as well as with SQL.</span>
-          <AboutButton
-            className="mt4 mb4"
-            buttonText="Explore Analytics APP"
-            link="https://www.footprint.network/dashboards"
-          />
-          <AboutImage className="About__demo-long-app-container" src={getOssUrl("home-v2/img_analytics_app.png")} alt="Analytics APP" />
-        </div>
-        <div className="About__demo-data-api">
-          <h3>Data API</h3>
-          <h4>Build your application with the Data API</h4>
-          <span>A unified data API for NFTs, GameFi, and DeFi across all major chain <br/>ecosystems. <br/>Support not only raw data but also statistics metrics with one line of code.</span>
-          <div className="About__demo-data-api-buttons">
-            <AboutButton
-              className="mt4 mb4"
-              buttonText="Explore Data API"
-              link="https://www.footprint.network/data-api"
-            />
-            <AboutButton
-              className="mt4 mb4"
-              buttonText="Batch Download"
-              link="https://www.footprint.network/batch-download"
-            />
-          </div>
-          <AboutImage className="About__demo-long-api-container" src={getOssUrl("home-v2/img_data_api_v2.png")} alt="Data API" />
-        </div>
+        {data.map(item => {
+          return (
+            <div key={item.title} className="About__demo-analytics-app" style={{ backgroundImage: `url("${getOssUrl(item.bg)}")`}}>
+              <h3>{item.title}</h3>
+              <h4>{item.desc}</h4>
+              {item.content}
+              <div className="About__demo-data-api-buttons">
+                {item.buttons.map(button => {
+                  return (
+                    <AboutButton
+                      key={button.buttonText}
+                      className="mt4 mb4"
+                      buttonText={button.buttonText}
+                      link={button.link}
+                    />
+                  )
+                })}
+              </div>
+              <AboutImage className="About__demo-long-app-container" src={getOssUrl(item.image)} alt="Research Portal" />
+            </div>
+          )
+        })}
       </div>
     </div>
   );
