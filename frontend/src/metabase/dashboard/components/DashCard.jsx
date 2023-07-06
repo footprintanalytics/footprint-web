9c97ecae7185923b125139cc843f6731b9d1a34b
duke <yudapei@gmail.com>
2023-07-06 15:13:02 +0800

    feat: refresh

--- frontend/src/metabase/dashboard/components/DashCard.jsx
+++ frontend/src/metabase/dashboard/components/DashCard.jsx
@@ -317,7 +317,7 @@ class DashCard extends Component {
 
     const showGetDataViaSqlApi = showEdit || showPreview;
     const showChartInfo = false;
-    const showChartRefresh = !isPublic;
+    const showChartRefresh = !isPublic && !showEdit && !singleDisplay;
     // const showStatusButton = isResearch;
     const showStatusButton = false;
 
