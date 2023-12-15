fe7085e0b28d0c9e4c69ed07ab27954494292624
dukelight <yudapei@gmail.com>
2023-12-15 09:29:50 +0800

    feat: combo (#1093)

--- frontend/src/metabase/query_builder/components/question/handle.js
+++ frontend/src/metabase/query_builder/components/question/handle.js
@@ -390,6 +390,15 @@ export function getChainDataList({ includeAll = true }) {
           protocolName: "The Sandbox",
         }
       },
+      {
+        value: "combo",
+        label: "Combo",
+        icon: getOssUrl("fp-chains/combo.png?image_process=resize,w_36/crop,h_36/format,webp"),
+        defaultProject: {
+          protocolSlug: "the-sandbox",
+          protocolName: "The Sandbox",
+        }
+      },
       /*{
         value: "wax",
         label: "Wax",
