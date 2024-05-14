717e86025f822bbe407ac99e4b9d54e0d718b70c
duke <yudapei@gmail.com>
2024-05-14 19:29:00 +0800

    feat: add grantedby

--- frontend/src/metabase/containers/aboutV2/components/AboutGrantedBy.jsx
+++ frontend/src/metabase/containers/aboutV2/components/AboutGrantedBy.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import { getOssUrl } from "metabase/lib/image";
+import { Link } from "react-router";
 
 const AboutGrantedBy = () => {
   const data = [
@@ -8,10 +9,16 @@ const AboutGrantedBy = () => {
     { image: "home-v2/img_by_cronos_v2.png?image_process=resize,w_330/crop,h_146/format,jpg", title: "cronos" },
     { image: "home-v2/img_by_optimism_v2.png?image_process=resize,w_330/crop,h_146/format,jpg", title: "optimism" },
     { image: "home-v2/img_by_sui.png?image_process=resize,w_330/crop,h_146/format,jpg", title: "sui" },
-    { image: "home-v2/img_by_combo.png?image_process=resize,w_330/crop,h_146/format,jpg", title: "combo" },
-    { image: "home-v2/img_by_dfinity.png?image_process=resize,w_330/crop,h_146/format,jpg", title: "dfinity" },
-    { image: "home-v2/img_by_nautilus.png?image_process=resize,w_330/crop,h_146/format,jpg", title: "nautilus" },
-    { image: "home-v2/img_by_rootstock.png?image_process=resize,w_330/crop,h_146/format,jpg", title: "rootstock" },
+    { image: "home-v2/img_by_combo.png?image_process=resize,w_330/crop,h_146/format,jpg", title: "combo", link: "https://combonetwork.io/all-apps" },
+    { image: "home-v2/img_by_dfinity.png?image_process=resize,w_330/crop,h_146/format,jpg", title: "dfinity", link: "https://dfinity.org/grants" },
+    { image: "home-v2/img_by_nautilus.png?image_process=resize,w_330/crop,h_146/format,jpg", title: "nautilus", link: "https://nautchain.xyz/#/ecosystem" },
+    { image: "home-v2/img_by_rootstock.png?image_process=resize,w_330/crop,h_146/format,jpg", title: "rootstock", link: "https://rootstock.io/ecosystem" },
+    { image: "home-v2/img_by_gala.jpg?image_process=resize,w_330/crop,h_146/format,jpg", title: "galachain" },
+    { image: "home-v2/img_by_starknet.jpg?image_process=resize,w_330/crop,h_146/format,jpg", title: "starknet", link: "https://www.starknet.io/en/ecosystem/block-explorers-indexers-and-enhanced-api" },
+    { image: "home-v2/img_by_tarko.jpg?image_process=resize,w_330/crop,h_146/format,jpg", title: "rootstock" },
+    { image: "home-v2/img_by_arb.jpg?image_process=resize,w_330/crop,h_146/format,jpg", title: "arbitrum", link: "https://portal.arbitrum.io/?project=footprint-analytics" },
+    { image: "home-v2/img_by_merlin.jpg?image_process=resize,w_330/crop,h_146/format,jpg", title: "merlin" },
+    { image: "home-v2/img_by_core.jpg?image_process=resize,w_330/crop,h_146/format,jpg", title: "corechain", link: "https://coredao.org/explore/ecosystem" },
   ];
 
   return (
@@ -21,7 +28,9 @@ const AboutGrantedBy = () => {
         {data.map(item => {
           return (
             <li key={item.title}>
-              <img src={getOssUrl(item.image)} alt={item.title}/>
+              <Link to={item.link} target="_blank">
+                <img src={getOssUrl(item.image)} alt={item.title}/>
+              </Link>
             </li>
           )
         })}
