5dd12bf31923153ce9c3dc8812de01496f27499c
duke <yudapei@gmail.com>
2024-06-20 16:52:31 +0800

    fix

--- frontend/src/metabase/submit/contract/components/RefContractTable.jsx
+++ frontend/src/metabase/submit/contract/components/RefContractTable.jsx
@@ -190,6 +190,28 @@ const RefContractTable = ({ data, recordType, showActionColumn = true, tableRowC
       },
     },
     {
+      title: "Decode Status",
+      render: (_, { decode_status, createdAt, error }) => {
+        const text = decode_status;
+        switch (text) {
+          case "holding":
+          case "pending":
+          case "decoding":
+            return <Tag color="processing">{text}</Tag>;
+          case "decoded_failed":
+            return <Tag color="error">{text}</Tag>;
+          case "decoded_completed":
+            return (
+              <Tag color="success">{text}</Tag>
+            );
+          default:
+            return (
+              <>{" "}</>
+            );
+        }
+      },
+    },
+    {
       title: "Submitted by",
       // width: 240,
       render: (_, record) => {
