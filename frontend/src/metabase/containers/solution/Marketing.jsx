d96ecd3e0e94cfef6a7605f12b4667ea1bafdc91
dukelight <yudapei@gmail.com>
2024-07-04 23:37:19 +0800

    feat: solution seo image (#1219)

--- frontend/src/metabase/containers/solution/Marketing.jsx
+++ frontend/src/metabase/containers/solution/Marketing.jsx
@@ -10,16 +10,18 @@ import HomeFooter from "metabase/containers/home/components/HomeFooter";
 import UserGrowth from "metabase/containers/solution/components/UserGrowth";
 import Engine from "metabase/containers/solution/components/Engine";
 import Drive from "metabase/containers/solution/components/Drive";
+import Meta from "metabase/components/Meta";
+import { getOssUrl } from "metabase/lib/image";
 
 const Marketing = () => {
-  // const defaultDesc =
-  //   "Download blockchain historical data in one batch. Start building DApps immediately, not in months. Built for money tracking, backtesting, machine learning, and more.";
-  // const keywords = "Footprint Analytics, historical data, blockchain data, cryptocurrency data, NFT data, Ethereum data, BNB chain data, polygon chain data, web3 gaming, web3 gaming analytics, web3 gaming data, NFT game, blockchain AML, wallet profile, Growth Marketing, cross chain data, blockchain data api";
-  // const title = "Download blockchain historical data in one batch| A faster and more affordable way to access blockchain data";
+  const defaultDesc =
+    "Comprehensive wallet analysis for precise, high-quality user acquisition and retention.\n";
+  const keywords = "Web3 Growth, User Acquisition, User Retention, Blockchain Data, Wallet Analysis, Token Analysis";
+  const title = "Tailored Blockchain Data Solution for User Acquisition | Footprint Analytics";
 
   return (
     <>
-      {/*<Meta description={defaultDesc} keywords={keywords} title={title} />*/}
+      <Meta description={defaultDesc} keywords={keywords} title={title} image={getOssUrl("home-v2/img-seo-Marketing.jpg", { resize: true })}/>
       <div className="solution__about">
         <BatchDownloadStart
           title={<>Unlock Your Potential With <br/>Data-Driven Customer<br/>Acquisition</>}
