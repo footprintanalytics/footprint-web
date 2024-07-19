e6809a9c5346e1c4ea9bc818617241b69ffcae91
dukelight <yudapei@gmail.com>
2024-07-19 18:09:36 +0800

    feat: open fga sdk (#1239)

--- frontend/src/metabase/lib/fgasdk.js
+++ frontend/src/metabase/lib/fgasdk.js
@@ -4,7 +4,7 @@ import FgaSDK from "fga-sdk-web";
 
 let fgaSDK = null;
 
-/*try {
+try {
   const SDK_PROJECT_ID = 752
   const SDK_TOKEN = "HJKDHIxgIeve/6+wmWfesfDd/idEGpjL6VOPN5IYIbLdFeQmt9gpDZEGKzu1T2fq"
   const fgaSDK = new FgaSDK({
@@ -14,7 +14,7 @@ let fgaSDK = null;
   window.fgaSDK = fgaSDK
 } catch (e) {
   console.log("e", e)
-}*/
+}
 
 export default fgaSDK;
 
