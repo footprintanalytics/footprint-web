419b6c6f373cfa8cb2ce9f181b7d172f6dbf7687
duke <yudapei@gmail.com>
2024-08-08 11:41:28 +0800

    feat: webapp

--- frontend/src/metabase/ab/app/hub.jsx
+++ frontend/src/metabase/ab/app/hub.jsx
@@ -4,49 +4,40 @@ import React, { useEffect, useState } from "react";
 import { compose } from "underscore";
 import MetaViewportControls from "metabase/dashboard/hoc/MetaViewportControls";
 import title from "metabase/hoc/Title";
-import { Segmented, Tabs, Skeleton } from "antd";
+import { Skeleton, Tabs } from "antd";
 import { push } from "react-router-redux";
 import { connect } from "react-redux";
 import { getUser } from "metabase/selectors/user";
 import { getPeaToken } from "metabase/selectors/control";
 import "./index.css";
 import PeaPage from "metabase/ab/containers/PeaPage";
-import { GiftOutlined, ShoppingOutlined, UserOutlined } from "@ant-design/icons";
 import { loginTelegram } from "metabase/auth/actions";
-import { getPeaTokenAPI } from "metabase/new-service";
-const { TabPane } = Tabs;
 
 const Hub = ({router, location, onChangeLocation, peaToken, loginTelegram}) => {
   const [height, setHeight] = useState(0);
   const tgWebAppStartParam = location?.query?.tgWebAppStartParam
+  const type = location?.query?.type
+  const peaHost = "https://test.pea.ai"
   console.log("tgWebAppStartParam", tgWebAppStartParam)
-  const [activeKey, setActiveKey] = useState("plaza");
-  const queryParams = new URLSearchParams(location.search);
-  const type = queryParams.get('type');
+  console.log("typetypetype", type)
   // const other = `app_name=fga&token=${peaToken || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NjgyNjkzMTdlZmYwMzAwMTE1MGU3MGUiLCJpYXQiOjE3MjI0ODE4MTAsImV4cCI6MTcyNTA3MzgxMH0.LlqLB3LSPEB8BBJVGlJ6MLAap1hj0zD_HChKvyon2hA"}`
   const other = `app_name=fga&token=${peaToken}`
   console.log("peaTokenpeaToken", peaToken)
-  const url = `https://test.pea.ai/app/quest/lucky_money?tgWebAppStartParam=${tgWebAppStartParam}&${other}`
-  const tgAppTabs = [
-    {
-      icon: <GiftOutlined />,
-      name: 'Plaza',
-      value: 'plaza',
-      url: `https://test.pea.ai/app?tab=plaza&${other}`
-    },
-    {
-      icon: <ShoppingOutlined />,
-      name: 'Community',
-      value: 'community',
-      url: `https://test.pea.ai/community?${other}`
-    },
-    {
-      icon: <UserOutlined />,
-      name: 'Account',
-      value: 'account',
-      url: `https://test.pea.ai/app?tab=account&${other}`
-    },
-  ]
+  const getQuestWebUrl = () => {
+    switch (type) {
+      case 'lucky_draw':
+        return `/app/campaign/lucky_draw`
+      case 'instant_draw':
+        return `/app/campaign/instant_draw`
+      case 'fission_lucky_money':
+        return `/app/quest/lucky_money`
+      case 'normal':
+      default:
+        return `/campaign/detail`
+    }
+  }
+  const questUrl = getQuestWebUrl(type)
+  const url = `${peaHost}${questUrl}?tgWebAppStartParam=${tgWebAppStartParam}&${other}`
 
   useEffect(() => {
     setTimeout(() => {
@@ -57,30 +48,6 @@ const Hub = ({router, location, onChangeLocation, peaToken, loginTelegram}) => {
       }
     }, 10)
   }, [])
-
-  useEffect(() => {
-    setActiveKey(type)
-  }, [type]);
-
-  const TabContent = () => {
-    const queryParams = new URLSearchParams(location.search);
-    const type = queryParams.get('type') || "plaza";
-    const url = tgAppTabs.find(i => i.value === type).url
-    if (!height) {
-      return <div>Loading...</div>
-    }
-    return (
-      <PeaPage
-        router={router}
-        location={location}
-        url={url}
-        outerIframeHeight={height}
-      />
-    );
-  };
-  const onChange = (value) => {
-    onChangeLocation(`/growth-fga/app?type=${value}`);
-  };
 
   if (!peaToken) {
     return (<div className={"full-width full-height p4"}><Skeleton /></div>)
