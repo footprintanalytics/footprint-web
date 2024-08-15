950dab6459626067b9a14dcd594bbda19507bb62
dukelight <yudapei@gmail.com>
2024-08-15 18:43:14 +0800

    fix (#1263)

--- frontend/src/metabase/containers/solution/components/BuildMagic.jsx
+++ frontend/src/metabase/containers/solution/components/BuildMagic.jsx
@@ -6,7 +6,7 @@ const BuildMagic = () => {
   return (
     <div className="solution__build-magic">
       <div className="solution__build-magic-head">
-        <div className="flex align-center"><h2>Building the Tools{" "}</h2><h3>That Build Tools</h3></div>
+        <div className="flex align-center"><h2>Building the Tools</h2><h3 style={{marginLeft: 6}}>That Build Tools</h3></div>
         <span>Intelligent data pipeline built on adaptable smart models.</span>
       </div>
       <div className="solution__build-magic-image"/>
