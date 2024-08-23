2728e1e21079a49f952068e6e9046569fc5cd3ba
dukelight <yudapei@gmail.com>
2024-08-23 18:50:34 +0800

    feat: growthly domain (#1268)

--- frontend/src/metabase/ab/utils/utils.tsx
+++ frontend/src/metabase/ab/utils/utils.tsx
@@ -488,5 +488,5 @@ export function getGAFavoritedTemplate() {
 }
 
 export function getPeaHost() {
-  return isProduction ? "https://app.pea.ai" : "https://test.pea.ai";
+  return isProduction ? "https://growthly.footprint.network" : "https://growthly-dev.footprint.network";
 }
