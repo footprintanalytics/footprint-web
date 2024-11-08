bcb55c733a12ec79177c107cdbc68d79caa11f5c
duke <yudapei@gmail.com>
2024-11-08 18:16:29 +0800

    fga flow

--- frontend/src/metabase/ab/components/GaProjectSearch.jsx
+++ frontend/src/metabase/ab/components/GaProjectSearch.jsx
@@ -55,7 +55,6 @@ const GaProjectSearch = props => {
     }
 
   const loadProjectDetail = projectId => {
-    console.log("loadProjectDetail9", projectId)
     if (isProFga) {
       loadCurrentFgaProjectById(projectId, from);
     } else {
@@ -101,7 +100,6 @@ const GaProjectSearch = props => {
       if (project.projectId !== 1) {
         setCurrentProject(project.projectName);
         saveLatestGAProject(project.projectName);
-        console.log("useEffect,loadProjectDetail", project.projectId, fromLeftMenu)
         loadProjectDetail(project.projectId);
 
         setUserProject(projects);
